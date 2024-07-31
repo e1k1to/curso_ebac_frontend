5b5ae72ca58f2830653484e21c87f6ca90b1960a
e1k1to <eikimillian@protonmail.com>
2024-07-31 16:21:31 -0300

    mais correcoes sobre a -> li

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
     $('form').submit(function(e) {
         e.preventDefault();
         const texto = $('#input-tarefa').val();
-        const tarefaNova = $("<li></li>");
-        novatf = $(`<a href="#" id="elem-clicavel"> ${texto} </a>`)
-        novatf.appendTo(tarefaNova);
+        const tarefaNova = $(`<li>${texto}</li>`);
         tarefaNova.appendTo($("ul"))
         $("#input-tarefa").val("");
     })
 
-    $("ul").on("click", "a", function(e) {
+    $("ul").on("click", "li", function(e) {
         e.preventDefault();
         $(this).addClass("marcador-feito");
     })
